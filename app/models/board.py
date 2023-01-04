e3592a366b1e20ebb4b1535f8e7d60b3a8d86c6c
Tanil <cadette.tanil@gmail.com>
2023-01-04 11:52:26 -0500

    board

--- app/models/board.py
+++ app/models/board.py
@@ -3,6 +3,7 @@ from app import db
 class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+    card= db.relationship("Card", back_populates="board")
     
     
     def to_dict(self):
