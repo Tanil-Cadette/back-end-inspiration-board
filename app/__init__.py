9f6108c355d756ba71ccdbafe1a039661e892e0e
Neida <neida@duck.com>
2023-01-05 20:57:34 -0500

    modifies create_app to handle testing

--- app/__init__.py
+++ app/__init__.py
@@ -10,23 +10,25 @@ migrate = Migrate()
 load_dotenv()
 
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/inspiration_board_api_development'
 
-    # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    #     "SQLALCHEMY_DATABASE_URI")
-
-    # Import models here for Alembic setup
-    # from app.models.ExampleModel import ExampleModel
-    from app.models.board import Board
-    from app.models.card import Card
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app, db)
+    
+    from app.models.board import Board
+    from app.models.card import Card
 
-    # Register Blueprints here
     from .board_routes import boards_bp
     app.register_blueprint(boards_bp)
     
